9ca77a92209a783398a9c8ea1ece5382cf4c03f1
douglas0100 <douglasfelipe0100@gmail.com>
2023-04-18 15:23:35 +0000

    send commit

---
+++ CamelCase.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class CamelCase {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static List<String> getCamelCase(String camelCase){
+        
+        char[] characterArray = camelCase.toCharArray();
+
+        for (char currentCharacter : characterArray) {
+            
+            if(Character.isUpperCase(currentCharacter)){
+                
+            }
+
+        }
+    }
+
+    
+}
